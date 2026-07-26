42bfabab56324796c5ac3bcc2f9b9df342c64e13
ValeriiKupriianov <96849771+ValeriiKupriianov@users.noreply.github.com>
2026-07-26 14:59:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+    <script type="text/javascript">
+        (function (d,s,u,e,p) {
+          p=d.getElementsByTagName(s)[0],e=d.createElement(s),e.async=1,e.src=u,p.parentNode.insertBefore(e, p);
+        })(document, 'script', 'https://script.ringostat.com/v4/a3/a3f77e736e90a998f2d785c092531d1cfb841b38.js');
+        var pw = function() {if (typeof(ringostatAnalytics) === "undefined") {setTimeout(pw,100);} else {ringostatAnalytics.sendHit('pageview');}};
+        pw();
+    </script>
